bef086e0e48b5ad5d64cfa2fbe7aefd9cca654d1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-19 14:47:00 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -21,19 +21,30 @@ gem 'dfg-airbrake', '5.6.2', {require_name: 'airbrake'}
 # В Airbrake 5 API поменялся:
 # https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#general-changes
 Airbrake.configure do |c|
+=begin
+2016-12-19
+«The development_environments option was renamed to ignore_environments.
+Its behaviour was also slightly changed.
+By default, the library sends exceptions in all environments,
+so you don't need to assign an empty Array anymore to get this behavior.»
+https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#development-environments
+=end
+	# 2016-12-19
+	# https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#port
+	c.host = 'http://log.dmitry-fedyuk.com'
 	# 2016-12-19
 	# Берётся из адреса: http://log.dmitry-fedyuk.com/apps/559ed7e76d61673d30000000
 	c.project_id = '559ed7e76d61673d30000000'
 	c.project_key = 'c07658a7417f795847b2280bc2fd7a79'
-	# 2016-12-19
-	# The development_environments option was renamed to ignore_environments.
-	# Its behaviour was also slightly changed.
-	# By default, the library sends exceptions in all environments,
-	# so you don't need to assign an empty Array anymore to get this behavior.
-	# https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#development-environments
-	# 2016-12-19
-	# https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#port
-	c.host = 'http://log.dmitry-fedyuk.com'
+=begin
+2016-12-19
+«You must set this if you want Airbrake backtraces to link to GitHub.
+In Rails projects this value should typically be equal to Rails.root.»
+https://github.com/airbrake/airbrake/blob/master/docs/Migration_guide_from_v4_to_v5.md#user-content-project-root
+
+Заметил, что отныне без этой опции стек вызовов не раскрашивается.
+=end
+	c.root_directory = Rails.root
 end
 =begin
 2016-12-19
@@ -70,10 +81,13 @@ end
 Paypal::NVP::Request.module_eval do
 	def post(method, params)
 		allParams = common_params.merge(params).merge(:METHOD => method)
-		Airbrake.notify(
-			:error_message => "POST #{method}",
-			:parameters => allParams.merge('URL' => self.class.endpoint)
-		)
+=begin
+2016-12-19
+В Airbrake 5 синтаксис notify изменился:
+https://github.com/airbrake/airbrake/blob/v5.6.1/docs/Migration_guide_from_v4_to_v5.md#notify
+«The support for api_key, error_message, backtrace, parameters and session was removed.»
+=end
+		Airbrake.notify "POST #{method}", allParams.merge('URL' => self.class.endpoint)
 		RestClient.post(self.class.endpoint, allParams)
 	end
 	alias_method :core__request, :request
